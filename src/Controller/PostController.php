92e0cd51a12b15a0a2188fde2b24a95368b7b251
JabriHassane <75222601+JabriHassane@users.noreply.github.com>
2021-10-28 12:39:55 +0100

    methode

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -5,8 +5,13 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 
@@ -47,5 +52,56 @@ class PostController
             [],
             true
         );
+    }
+
+    /**
+     * @Route(name="api_posts_collection_post", methods={"POST"})
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
+     */
+    public function post(Request  $request,
+                         SerializerInterface $serializer,
+                         EntityManagerInterface $entityManager,
+                         UrlGeneratorInterface $urlGenerator
+    ): JsonResponse {
+        /**
+         * @var Post $post
+         */
+        $post = $serializer->deserialize($request->getContent(), Post::class, 'json');
+        $post->setAuthor($entityManager->getRepository(User::class)->findOneBy([]));
+        $entityManager->persist($post);
+        $entityManager->flush();
+
+        return new JsonResponse(
+            $serializer->serialize($post, "json", ["groups"=>"get"]),
+            JsonResponse::HTTP_OK,
+            ["Location"=> $urlGenerator->generate("api_posts_item_get", ["id" => $post->getId()])],
+            true
+        );
+    }
+
+    /**
+     * @Route("/{id}", name="api_posts_item_put", methods={"PUT"})
+     * @param Post $post
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $entityManager
+     * @return JsonResponse
+     */
+    public function put(
+        Post $post,
+        Request $request,
+        SerializerInterface $serializer,
+        EntityManagerInterface $entityManager
+    ): JsonResponse{
+        $serializer->deserialize(
+            $request->getContent(),
+            Post::class,
+            'json',
+            [AbstractNormalizer::OBJECT_TO_POPULATE=>$post]
+        );
+        $entityManager->flush();
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
     }
 }
